5bf6c51793df43d46e135218c705628506f8164e
Ellie421 <goodellie0421@gmail.com>
2025-09-05 17:51:41 +0900

    Update professor.controller.js

--- src/controllers/professor.controller.js
+++ src/controllers/professor.controller.js
@@ -25,11 +25,11 @@ const postProfessorReview = async (req, res, next) => {
     const { professor_course_id } = req.params;
     const { content } = req.body;
 
-    const newReview = await professorService.postProfessorReview(
+    const new_review = await professorService.postProfessorReview(
       professor_course_id,
       content
     );
-    return res.success(newReview, StatusCodes.OK);
+    return res.success(new_review, StatusCodes.OK);
   } catch (error) {
     next(error);
   }
